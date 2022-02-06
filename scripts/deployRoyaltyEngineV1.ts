643c6218c87e07f5401bd5e5473855a323df55c6
Nejc Drobnic <ethoarx@gmail.com>
2022-02-06 03:03:11 +0100

    feat: deploy engine to boba mainnet

---
+++ scripts/deployRoyaltyEngineV1.ts
@@ -0,0 +1,30 @@
+import { ethers, upgrades } from 'hardhat';
+import type { RoyaltyEngineV1 } from '../typechain';
+
+const ROYALTY_REGISTRY_ADDRESS = '0xF65F2242f4311A78D737d9234d79180116A81a42';
+
+async function main() {
+	const [deployer] = await ethers.getSigners();
+
+	const RoyaltyEngineV1Contract = await ethers.getContractFactory('RoyaltyEngineV1');
+	const royaltyEngineV1 = (await upgrades.deployProxy(RoyaltyEngineV1Contract, [ROYALTY_REGISTRY_ADDRESS], {
+		initializer: '__RoyaltyEngineV1_init',
+		kind: 'transparent'
+	})) as RoyaltyEngineV1;
+	await royaltyEngineV1.deployed();
+
+	console.log(
+		[
+			`Deployed:` /**/, //
+			` - "RoyaltyEngineV1" deployed to ${royaltyEngineV1.address}`,
+			` - Deployer address is ${deployer.address}`
+		].join('\n')
+	);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
